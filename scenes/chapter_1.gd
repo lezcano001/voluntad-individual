0dfa1dce38b859afe530e1e5d148a4613ef75d14
Elias Lezcano <eliaslezcano001@gmail.com>
2023-08-07 16:07:36 -0400

    Solve kevin's error, he deleted the velocity update on the chapter 1

--- scenes/chapter_1.gd
+++ scenes/chapter_1.gd
@@ -1,3 +1,7 @@
 extends Node2D
 
-const WEIGHT = 300
+@onready var ship := $Ship
+@onready var shipControl := $ShipControl/ShipControlPane
+
+func _on_ship_speed_updated(speed):
+	ship.speed = speed
